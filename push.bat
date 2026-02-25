ec1f104e27846890afc15b31de2d83c25426c510
CursoOMT <omt-curso@example.com>
2026-02-24 19:15:36 -0500

    Logo PNG transparente centrado + sin marco negro

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 set PATH=C:\Program Files\Git\cmd;%PATH%
 git add -A
-git commit -m "Logo de academia + eliminar duracion del splash"
+git commit -m "Logo PNG transparente centrado + sin marco negro"
 git push origin main
 echo PUSH COMPLETE
